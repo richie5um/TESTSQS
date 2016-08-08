ca6ab1b791e91912e1b6872b08f3e936dc2a7f70
Rich Somerfield <rs@richsomerfield.com>
2016-08-08 10:19:11 +0100

    added options to receive

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 'use strict';
 
+var _ = require('lodash');
 var AWS = require('aws-sdk');
 var FS = require('fs');
 var Promise = require('bluebird');
@@ -15,17 +16,26 @@ var awsConfig = JSON.parse(FS.readFileSync(awsConfigPath));
 sendMessage({ key: 'seed', value: 1 });
 listener();
 
-function receiveMessage() {
+function receiveMessages(options) {
+    var defaultOptions = {
+        MaxNumberOfMessages: 10,
+        VisibilityTimeout: 60,
+        WaitTimeSeconds: 20
+    };
+
+    options = _.merge(defaultOptions, options);
+
     return new Promise(function (resolve, reject) {
         sqs.receiveMessage({
             QueueUrl: awsConfig.sqsQueueUrl,
-            MaxNumberOfMessages: 10,
-            VisibilityTimeout: 60,
-            WaitTimeSeconds: 20
+            MaxNumberOfMessages: options.MaxNumberOfMessages,
+            VisibilityTimeout: options.VisibilityTimeout,
+            WaitTimeSeconds: options.WaitTimeSeconds
         }, function (err, data) {
             if (err) {
                 return reject(err);
             }
+            console.log("Received:", data.Messages.length);
             return resolve(data);
         });
     });
@@ -47,6 +57,7 @@ function sendMessage(body) {
 
 function deleteMessage(message) {
     return new Promise(function (resolve, reject) {
+        console.log("Deleting: ", message.ReceiptHandle);
         sqs.deleteMessage({
             QueueUrl: awsConfig.sqsQueueUrl,
             ReceiptHandle: message.ReceiptHandle
@@ -68,25 +79,23 @@ function processMessage(message) {
 };
 
 function listener() {
-    receiveMessage().then(function (data) {
-        return Promise
-            .all(data.Messages.map(function (message) {
-                var origMessage = message;
-                return processMessage(message)
-                    .then(function (body) {
-                        console.log({ old: origMessage.Body, new: JSON.stringify(body) });
-                        return body;
-                    })
-                    .then(function (body) {
-                        return sendMessage(body)
-                    })
-                    .finally(function () {
-                        return deleteMessage(origMessage);
-                    });
-                })
-            )
-            .finally(function () {
-                listener();
+    receiveMessages({ MaxNumberOfMessages: 3 }).then(function (data) {
+        return Promise.all(data.Messages.map(function (message) {
+            var origMessage = message;
+            return processMessage(message).then(function (body) {
+                console.log({ old: origMessage.Body, new: JSON.stringify(body) });
+                return body;
+            }).then(function (body) {
+                return sendMessage(body)
+            }).catch(function (err) {
+                console.log(err);
+            }).finally(function () {
+                return deleteMessage(origMessage);
             });
+        })).finally(function () {
+            listener();
+        }).catch(function (err) {
+            console.log(err);
+        });
     });
 };
